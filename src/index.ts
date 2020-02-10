7d4fb6a32963c893e7e7bfc37403e3d038296039
eniallator <eniallatormc@gmail.com>
2020-02-10 14:55:56 +0000

    Made the hue incrementing into 1 line

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ let hue: number = 0;
 function main() {
   $("#js-main-span").css("color", `hsl(${hue}, 100%, 50%)`);
 
-  hue++;
-  hue %= 360;
+  hue = ++hue % 360;
 
   requestAnimationFrame(main);
 }
